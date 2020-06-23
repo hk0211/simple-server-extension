2f94dbcfb397e6b534c7726e767d9cee1d137f1e
hk0211 <46514350+hk0211@users.noreply.github.com>
2020-06-23 22:16:17 +0900

    add kernel connector

--- labextension/index.js
+++ labextension/index.js
@@ -1,16 +1,39 @@
+import { KernelAPI, KernelManager, KernelMessage } from '@jupyterlab/services';
+
 const extension = {
     id: 'simple-lab-extension',
     autoStart: true,
-    activate: (app) => {
+    activate: async (app) => {
         const updateData = () => {
             const xhr = new XMLHttpRequest()
             xhr.open('POST', '/simple-server-extension', true);
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.send(JSON.stringify({"foo": "bar"}));
         };
+        await accessKernel();
         updateData();
         console.log('updateData() completed.');
     }
+}
+
+async function accessKernel() {
+  let kernelManager = new KernelManager();
+  let kernelModels = await KernelAPI.listRunning();
+  console.log(kernelModels);
+  if (kernelModels.length > 0) {
+    console.log(`Connecting to ${kernelModels[0].name}`);
+    const kernel = kernelManager.connectTo({ model: kernelModels[0] });
+
+    console.log('Executing code');
+    let future = kernel.requestExecute({ code: 'token = "asdf"' });
+    future.onIOPub = msg => {
+      if (msg.header.msg_type !== 'status') {
+        console.log(msg.content);
+      }
+    };
+    await future.done;
+    console.log('Execution is done');
+  }
 }
 
 export default extension;    
